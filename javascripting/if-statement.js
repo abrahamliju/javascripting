8669b0acefc4ff2f61e7b5899b4689b69a99c712
Liju <abrahamliju@live.com>
2016-12-08 04:49:48 +0000

    Intial commit with all my work so far

---
+++ javascripting/if-statement.js
@@ -0,0 +1,10 @@
+var fruit = 'orange' , n;
+n = fruit.length;
+if(n > 5)
+{
+  console.log('The fruit name has more than five characters.');
+}
+else
+{
+  console.log('The fruit name has five or less characters.');
+}
